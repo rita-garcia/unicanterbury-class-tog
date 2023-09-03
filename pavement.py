1b8b66ec8c981d85e898be1ac1f45865c7d6c16b
Rita Garcia <rita.garcia@unity3d.com>
2023-09-04 07:16:19 +1200

    Fixing class name

--- pavement.py
+++ pavement.py
@@ -36,7 +36,7 @@ if master_url is None:
     master_url = get_master_url()
 
 master_app = 'runestone'
-serving_dir = "./build/class-tog"
+serving_dir = "./build/uc-class-tog"
 
 #new 7/2019 changes
 # Change to False when running localhost
@@ -56,9 +56,9 @@ options(
     sphinx = Bunch(docroot=".",),
 
     build = Bunch(
-        builddir="./build/class-tog",
+        builddir="./build/uc-class-tog",
         sourcedir="_sources",
-        outdir="./build/class-tog",
+        outdir="./build/uc-class-tog",
         confdir=".",
         project_name=project_name,
         template_args={'course_id': project_name,
@@ -69,7 +69,7 @@ options(
                        'use_services': 'true',
                        'python3': 'true',
                        'dburl': 'postgresql://runestone@localhost/runestone',
-                       'basecourse': 'class-tog',
+                       'basecourse': 'uc-class-tog',
                         # new 7/2019 changes
                        'dynamic_pages': dynamic_pages,
                        'downloads_enabled': 'false',
